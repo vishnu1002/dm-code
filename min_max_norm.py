5abb5dcfaee218968dcce76bfddb891ad83d74ae
Vishnu <145321614+vishnu1002@users.noreply.github.com>
2024-02-03 08:43:50 +0530

    Update min_max_norm.py

--- min_max_norm.py
+++ min_max_norm.py
@@ -1,8 +1,27 @@
-from sklearn import preprocessing
+#manual:
 import numpy as np
-X_train = np.array([[ 1., -1.,  2.],
-[ 2.,  0.,  0.],
-[ 0.,  1., -1.]])
-min_max_scaler = preprocessing.MinMaxScaler()
-X_train_minmax = min_max_scaler.fit_transform(X_train)
-print(X_train_minmax)
+
+def min_max_scale(X):
+    """Scales features of X to a given range [0, 1] using min-max normalization."""
+    min_max_scaler = lambda x: (x - np.min(x)) / (np.max(x) - np.min(x))
+    X_scaled = min_max_scaler(X)
+    return X_scaled
+
+# Example usage:
+data = np.array([[10, 20, 30], [40, 50, 60], [70, 80, 90]])
+normalized_data = min_max_scale(data)
+print(normalized_data)
+
+
+#scikit
+from sklearn import preprocessing
+
+# Create a MinMaxScaler object
+scaler = preprocessing.MinMaxScaler()
+
+# Fit the scaler to the data
+scaler.fit(data)
+
+# Transform the data using the fitted scaler
+normalized_data = scaler.transform(data)
+print(normalized_data)
